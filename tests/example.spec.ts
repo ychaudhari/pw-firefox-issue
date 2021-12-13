a52c4f88408aeb86da60de06fb9ff0ad76293c23
Yogesh Chaudhari <yogesh.chaudhari@gmail.com>
2021-12-13 23:15:05 +1300

    trying on different computers

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test1', async ({ page }) => {
+  console.log('test 1');
   await page.goto('https://google.com/');
 });
 
 test('basic test2', async ({ page }) => {
+  console.log('test 2');
   await page.goto('https://google.com/');
 });
 
 test('basic test3', async ({ page }) => {
+  console.log('test 3');
+  await page.goto('https://google.com/');
+});
+
+test('basic test4', async ({ page }) => {
+  console.log('test 4');
   await page.goto('https://google.com/');
 });
